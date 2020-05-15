e2aaffb2e5241b908c4ad0604f11b221704a102d
Geov99 <george.geov7@gmail.com>
2020-05-15 22:33:33 +0300

    adaugare layout fara promisiuni, incepere admin layout

--- app.js
+++ app.js
@@ -38,19 +38,6 @@ conn.connect(function (err) {
 
 /* < ------------------------------------------------- PARTEA BD ------------------------------------------------- > */
 
-app.get('/', (req, res) => {
-  sess_id = req.session._id;
-  sess_uname = req.session.uname;
-  sess_rol = req.session.rol;
-
-  if(sess_rol == 1)
-  {
-    res.render("index", { layout: 'layoutIndex' })
-  }
-
-  console.log(sess_uname);
-  res.send("da");
-});
 
 /* < ------------------------------------------------- LAYOUT AUTH ------------------------------------------------- > */
 
@@ -109,32 +96,13 @@ app.post('/login', (req, res) => {
       bcrypt.compare(req.body['password'], result[0].password, function (err, response) {
         if (response) {
           res.clearCookie("mesajEroare");
-
-          // req.session.currentUser = [];
-
-          // let promise = new Promise((resolve, reject) => {
-          //   req.session.currentUser._id = result[0].id;
-          //   req.session.currentUser.uname = result[0].username;
-          //   req.session.currentUser.rol = result[0].rol;
-          //   resolve("done!");
-          // });
-          
-          // promise.then((value) => {
-          //   req.session.save(function (err) {
-          //     // session saved
-          //     console.log(value);
-          //     console.log("Session Before Redirect: ", req.session);
-          //     res.redirect("/");
-          //   });
-          // });
-          
           req.session._id = result[0].id;
           req.session.uname = result[0].username;
           req.session.rol = result[0].rol;
 
-            req.session.save(function (err) {
-              res.redirect("/");
-            });
+          req.session.save(function (err) {
+            res.redirect("/");
+          });
 
         } else {
           res.cookie("mesajEroare", 'Credentiale gresite!');
@@ -149,18 +117,206 @@ app.post('/login', (req, res) => {
   });
 });
 
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.redirect('/');
+  });
+
+});
+
 /* < ------------------------------------------------- LAYOUT AUTH ------------------------------------------------- > */
 
 /* < ------------------------------------------------- LAYOUT INDEX ------------------------------------------------- > */
 
-app.get('/index', (req, res) => {
+app.get('/', (req, res) => {
   sess_id = req.session._id;
   sess_uname = req.session.uname;
   sess_rol = req.session.rol;
 
-  res.render('index', { layout: 'layoutIndex', locals: { rol: sess_rol } });
+  if (sess_rol && sess_id && sess_uname) {
+
+    // conn.query('SELECT * FROM devices WHERE user = ?', sess_id, (err, result, fields) => {
+
+    //   if (err) throw err;
+
+    //   var devices = [];
+
+    //   result.forEach(function (row) {
+    //     if (row['device_type'] == 'Computer' || row['device_type'] == ['Printer']) {
+    //       let newRow = {};
+    //       newRow.MAC = row['MAC'];
+    //       newRow.IP = row['IP'];
+    //       newRow.device_type = row['device_type'];
+    //       newRow.OS = row['OS'];
+    //       devices.push(newRow);
+
+    //     } else {
+    //       conn.query('SELECT AP_ESSID FROM access_point WHERE MAC = ?', row['MAC'], (err, ap_essid) => {
+    //         if (err) throw err;
+
+    //         conn.query('SELECT SSID FROM access_point_details WHERE AP_ESSID = ?', ap_essid[0].AP_ESSID , (err, ssid) => {
+
+    //           let newRow = {};
+    //           newRow.MAC = row['MAC'];
+    //           newRow.IP = row['IP'];
+    //           newRow.device_type = row['device_type'];
+    //           newRow.SSID = ssid;
+    //           newRow.OS = row['OS'];
+    //           devices.push(newRow);
+    //         });
+
+    //       });
+    //     }
+
+    //   });
+
+    //   for(let i in devices)
+    //   {
+    //     console.log(devices[i]);
+    //   }
+
+    //   if (sess_rol == 1) {
+    //     res.render('index', { layout: 'layout', locals: { _id: sess_id, uname: sess_uname, rol: sess_rol } });
+    //   }
+    // });
+
+      conn.query('SELECT * FROM devices WHERE user = ?', sess_id, (err, result, fields) => {
+        if (err) throw err;
+
+        var devices = [];
+
+        const promise = new Promise(function (resolve, reject) {
+
+          result.forEach(function (row) {
+            if (row['device_type'] == 'Computer' || row['device_type'] == ['Printer']) {
+              let newRow = {};
+              newRow.MAC = row['MAC'];
+              newRow.IP = row['IP'];
+              newRow.device_type = row['device_type'];
+              newRow.OS = row['OS'];
+              devices.push(newRow);
+              console.log("intra x1");
+            } else {
+              conn.query('SELECT AP_ESSID FROM access_point WHERE MAC = ?', row['MAC'], (err, ap_essid) => {
+                if (err) throw err;
+                conn.query('SELECT SSID FROM access_point_details WHERE AP_ESSID = ?', ap_essid[0].AP_ESSID, (err, ssid) => {
+                  let newRow = {};
+                  newRow.MAC = row['MAC'];
+                  newRow.IP = row['IP'];
+                  newRow.device_type = row['device_type'];
+                  newRow.SSID = ssid[0].SSID;
+                  newRow.OS = row['OS'];
+                  devices.push(newRow);
+                  console.log("intra x2");
+                });
+              });
+            }
+          });
+          resolve(devices);
+
+        }).then(function (value) {
+          console.log("aia e");
+      });
+
+    });
+
+
+    res.render('index', { layout: 'layout', locals: { _id: sess_id, uname: sess_uname, rol: sess_rol } });
+
+  } else {
+    res.redirect('/login');
+  }
+
+});
+
+app.get('/add-device', (req, res) => {
+  sess_id = req.session._id;
+  sess_uname = req.session.uname;
+  sess_rol = req.session.rol;
+
+  if (sess_rol && sess_id && sess_uname) {
+
+    if(req.cookies.mesajInsert) {
+      mesaj = req.cookies.mesajInsert;
+    } else {
+      mesaj = " ";
+    }
+
+    res.render('add-device', { layout: 'layout', locals: { _id: sess_id, uname: sess_uname, rol: sess_rol, mesaj: mesaj } });
+  } else {
+    res.redirect('/login');
+  }
+
+});
+
+app.post('/add-device', (req, res) => {
+  var MAC_flag = 0;
+  var IP_flag = 0;
+
+  var p1 = new Promise(function(resolve, reject) {
+    conn.query('SELECT MAC FROM devices WHERE MAC = ?', req.body['deviceMAC'], (err, MAC) => {
+      if (err) throw err;
+      if (MAC != "") {
+        MAC_flag = 1;
+        res.cookie("mesajEroareInsert", 'MAC already exist');
+      } else {
+        MAC_flag = 0;
+      }
+      resolve(MAC_flag);
+    });
+  });
+
+  var p2 = new Promise(function (resolve, reject) {
+    conn.query('SELECT IP FROM devices WHERE IP = ?', req.body['deviceIP'], (err, IP) => {
+      if (err) throw err;
+      if (IP != "") {
+        IP_flag = 1;
+        res.cookie("mesajInsert", 'IP already exists');
+      } else {
+        IP_flag = 0;
+      }
+      return resolve(IP_flag);
+    });
+  });
+
+  Promise.all([p1, p2]).then(flag => {
+
+      if(flag[0] == 1 && flag[1] == 1) // MAC_flag == 1 && IP_flag == 1
+      {
+        res.cookie("mesajInsert", 'This combination of MAC & IP already exists');
+        res.redirect('/add-device');
+      } else if(flag[0] == 1 || flag[1] == 1)
+      {
+        res.redirect('/add-device');
+      } else{
+        let values = [[req.body['deviceMAC'], req.body['deviceIP'], req.body['deviceType'], req.body['userOS'], req.session._id]];
+        conn.query("INSERT INTO devices(MAC, IP, device_type, OS, user) VALUES ?", [values], function (err, result, fields) {
+          if (err) throw err;
+          res.cookie("mesajInsert", 'Device added in our permitted devices list');
+          res.redirect('/add-device');
+        });
+      }
+
+    }, reason => {
+      console.log(reason)
+    });
+
 });
 
 /* < ------------------------------------------------- LAYOUT INDEX ------------------------------------------------- > */
+
+/* < ------------------------------------------------- LAYOUT ADMIN PANEL ------------------------------------------------- > */
+
+app.get('/admin-panel', (req, res) => {
+
+  console.log("mare pizdar tată");
+
+  res.render('admin-panel', { layout: 'layoutAdmin', locals: { _id: sess_id, uname: sess_uname, rol: sess_rol } });
+});
+
+/* < ------------------------------------------------- LAYOUT ADMIN PANEL ------------------------------------------------- > */
 
 app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:`));
